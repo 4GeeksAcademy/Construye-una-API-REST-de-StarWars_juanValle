7b91fb566be0770a85c67ac49a3ab5bf964b0f76
Juan Pablo <120145816+Dev-JuanPa@users.noreply.github.com>
2025-07-06 19:46:04 +0000

    planet and spaceship models

--- src/models.py
+++ src/models.py
@@ -1,15 +1,16 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
+from sqlalchemy import String, Boolean, Integer
 from sqlalchemy.orm import Mapped, mapped_column
 
 db = SQLAlchemy()
 
+
 class User(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
-    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+    email: Mapped[str] = mapped_column(
+        String(120), unique=True, nullable=False)
     password: Mapped[str] = mapped_column(nullable=False)
     is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
-
 
     def serialize(self):
         return {
@@ -17,3 +18,40 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+    class Planet (db.Model):  # Representa la tabla planet en mi base de datos
+
+        __tablename__ = "planet"
+
+        id: Mapped[int] = mapped_column(primary_key=True)
+        name: Mapped[str] = mapped_column(
+            String(120), unique=True, nullable=False)
+        climate: Mapped[str] = mapped_column(String(80), nullable=True)
+        population: Mapped[int] = mapped_column(Integer, nullable=True)
+
+        def serialize(self):
+            return {
+                "id": self.id,
+                "name": self.name,
+                "climate": self.climate,
+                "population": self.population
+            }
+
+        class Spaceship (db.Model):
+
+            __tablename__ = "spaceship"
+
+            id: Mapped[int] = mapped_column(primary_key=True)
+            name: Mapped[str] = mapped_column(
+                String(120), unique=True, nullable=False)
+            model: Mapped[str] = mapped_column(String(80), nullable=True)
+            manufacturer: Mapped[int] = mapped_column(
+                String(80), nullable=True)
+
+            def serialize(self):
+                return {
+                    "id": self.id,
+                    "name": self.name,
+                    "model": self.model,
+                    "manufacturer": self.manufacturer
+                }
